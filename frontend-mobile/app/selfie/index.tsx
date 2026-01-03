a7f3a3343003e58dd38faaa47437fe4dbd8cd7e8
Sophia <kenza.rahmoun@student.ehb.be>
2026-01-03 23:12:11 +0100

    Fix: removed double import

--- frontend-mobile/app/selfie/index.tsx
+++ frontend-mobile/app/selfie/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-import React, { useState, useRef, useEffect } from "react";
 import { useCameraPermissions, CameraView } from "expo-camera";
 import {
 	View,
